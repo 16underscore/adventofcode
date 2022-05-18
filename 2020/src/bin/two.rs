348bee4841ebbdefdac5394d69345e44a1c48937
16underscore <steffen.gminsky@online.de>
2022-05-18 19:10:38 +0200

    Complete 2020 Day 2 Part 1

--- 2020/src/bin/two.rs
+++ 2020/src/bin/two.rs
@@ -7,5 +7,26 @@ fn main() {
 }
 
 fn part1(rows: &Vec<&str>) {
-	
+	let mut valid_passwords: u16 = 0;
+	for &row in rows {
+		let row: Vec<&str> = row.split_whitespace().collect();
+		let numbers: Vec<u8> = row
+			.get(0)
+			.unwrap()
+			.split('-')
+			.map(|x| x.parse().unwrap())
+			.collect();
+		let letter = row.get(1).unwrap().get(0..1).unwrap();
+		let password = row.get(2).unwrap();
+		let mut count = 0;
+		for char in password.chars() {
+			if letter.contains(char) {
+				count += 1;
+			}
+		}
+		if count >= *numbers.get(0).unwrap() && count <= *numbers.get(1).unwrap() {
+			valid_passwords += 1;
+		}
+	}
+	println!("The result for the first part is: {}", valid_passwords);
 }
