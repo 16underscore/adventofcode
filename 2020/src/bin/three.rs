796daefd279db542b5104afa5028210285da8761
16underscore <steffen.gminsky@online.de>
2022-05-19 10:39:50 +0200

    Complete 2020 Day 3 Part 2

--- 2020/src/bin/three.rs
+++ 2020/src/bin/three.rs
@@ -4,21 +4,41 @@ fn main() {
 	let input = fs::read_to_string("2020/three").unwrap();
 	let rows: Vec<&str> = input.split_whitespace().collect();
 	part1(&rows).unwrap();
+	part2(&rows).unwrap();
 }
 
 fn part1(rows: &Vec<&str>) -> Option<()> {
+	println!(
+		"The result for the first part is: {}",
+		traverse(rows, 3, 1)?
+	);
+	Some(())
+}
+
+fn part2(rows: &Vec<&str>) -> Option<()> {
+	let mut product: u32 = 1;
+	product *= traverse(rows, 1, 1)?;
+	product *= traverse(rows, 3, 1)?;
+	product *= traverse(rows, 5, 1)?;
+	product *= traverse(rows, 7, 1)?;
+	product *= traverse(rows, 1, 2)?;
+	println!("The result for the second part is: {}", product);
+	Some(())
+}
+
+fn traverse(rows: &Vec<&str>, right: usize, down: usize) -> Option<u32> {
 	let mut position: usize = 0;
-	let mut counter = 0;
-	for row in rows {
-		if position >= row.len() {
-			position -= row.len();
+	let mut counter: u32 = 0;
+	for i in (0..rows.len()).step_by(down) {
+		let row_len = rows.get(i)?.len();
+		if position >= row_len {
+			position -= row_len;
 		}
-		let row: Vec<char> = row.chars().collect();
+		let row: Vec<char> = rows.get(i)?.chars().collect();
 		if row.get(position)?.eq(&'#') {
 			counter += 1;
 		}
-		position += 3;
+		position += right;
 	}
-	println!("The result for the second part is: {}", counter);
-	Some(())
+	Some(counter)
 }
