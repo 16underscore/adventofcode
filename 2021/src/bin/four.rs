f096b41cc20afce616598c2c3527fa043a8728f7
16underscore <steffen.gminsky@online.de>
2022-05-22 11:56:37 +0200

    WIP 2021 Day 4

--- 2021/src/bin/four.rs
+++ 2021/src/bin/four.rs
@@ -3,7 +3,7 @@ use std::fs;
 fn main() {
 	let input = fs::read_to_string("2021/four").unwrap();
 	let mut bingo: Vec<&str> = input.split("\n\n").collect();
-	let bingo_numbers: Vec<u8> = bingo
+	let _bingo_numbers: Vec<u8> = bingo
 		.remove(0)
 		.split(',')
 		.map(|num| num.parse().unwrap())
