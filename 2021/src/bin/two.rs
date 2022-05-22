13b031d8faa47a31216084fc93e88ad213817abb
16underscore <steffen.gminsky@online.de>
2022-05-22 11:56:21 +0200

    Improve 2021 Day 2

--- 2021/src/bin/two.rs
+++ 2021/src/bin/two.rs
@@ -2,32 +2,34 @@ use std::fs;
 
 fn main() {
 	let input: String = fs::read_to_string("2021/two").unwrap();
-	part1(&input);
-	part2(&input);
+	let rows: Vec<&str> = input.split('\n').collect();
+	part1(&rows).unwrap();
+	part2(&rows).unwrap();
 }
 
-fn part1(input: &String) {
+fn part1(rows: &Vec<&str>) -> Option<()> {
 	let (mut horizontal, mut depth) = (0, 0);
-	input.split('\n').for_each(|x| {
+	for x in rows {
 		let pair: Vec<&str> = x.split_whitespace().collect();
-		let direction: &str = pair.get(0).unwrap();
-		let value: i32 = pair.get(1).unwrap().parse().unwrap();
+		let direction: &str = pair.get(0)?;
+		let value: i32 = pair.get(1)?.parse().unwrap();
 		match direction {
 			"forward" => horizontal += value,
 			"down" => depth += value,
 			"up" => depth -= value,
 			_ => println!("This should not happen"),
 		}
-	});
+	}
 	println!("The result for the first part is: {}", horizontal * depth);
+	Some(())
 }
-fn part2(input: &String) {
-	let (mut horizontal, mut depth) = (0, 0);
-	let mut aim = 0;
-	input.split('\n').for_each(|x| {
+
+fn part2(rows: &Vec<&str>) -> Option<()> {
+	let (mut horizontal, mut depth, mut aim) = (0, 0, 0);
+	for x in rows {
 		let pair: Vec<&str> = x.split_whitespace().collect();
-		let direction: &str = pair.get(0).unwrap();
-		let value: i32 = pair.get(1).unwrap().parse().unwrap();
+		let direction: &str = pair.get(0)?;
+		let value: i32 = pair.get(1)?.parse().unwrap();
 		match direction {
 			"forward" => {
 				horizontal += value;
@@ -37,6 +39,7 @@ fn part2(input: &String) {
 			"up" => aim -= value,
 			_ => println!("This should not happen"),
 		}
-	});
+	}
 	println!("The result for the second part is: {}", horizontal * depth);
+	Some(())
 }
