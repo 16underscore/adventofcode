f6b81f24c2a47dc3b64e0f3875dbfee7bad23c1c
16underscore <steffen.gminsky@online.de>
2022-05-23 14:32:56 +0200

    Complete 2015 Day 1 Part 2

--- 2015/src/bin/one.rs
+++ 2015/src/bin/one.rs
@@ -3,6 +3,7 @@ use std::fs;
 fn main() {
 	let input = fs::read_to_string("2015/one").unwrap();
 	part1(&input);
+	part2(&input);
 }
 
 fn part1(input: &String) {
@@ -16,4 +17,21 @@ fn part1(input: &String) {
 		}
 	}
 	println!("The result for the first part is: {}", counter);
+}
+
+fn part2(input: &String) {
+	let mut counter: i16 = 0;
+	for i in 0..input.len() {
+		match input.get(i..i + 1).unwrap() {
+			"(" => counter += 1,
+			")" => {
+				if counter == 0 {
+					println!("The result for the second part is: {}", i + 1);
+					return;
+				}
+				counter -= 1
+			}
+			_ => panic!(""),
+		}
+	}
 }
