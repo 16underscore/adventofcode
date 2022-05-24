499e5c3dc9c9c3d2f3ada7c5a672e15924fb98aa
16underscore <steffen.gminsky@online.de>
2022-05-24 19:57:24 +0200

    Complete 2015 Day 3 Part 2

--- 2015/src/bin/three.rs
+++ 2015/src/bin/three.rs
@@ -3,23 +3,33 @@ use std::{collections::HashSet, fs};
 fn main() {
 	let input = fs::read_to_string("2015/three").unwrap();
 	part1(&input);
+	part2(&input);
 }
 
 fn part1(input: &String) {
-	let characters = input.chars();
+	println!("The result for the first part is: {}", deliver(input, 1));
+}
+
+fn part2(input: &String) {
+	println!("The result for the second part is: {}", deliver(input, 2));
+}
+
+fn deliver(characters: &String, santas: usize) -> usize {
 	let mut houses: HashSet<(i16, i16)> = HashSet::new();
-	let mut x: i16 = 0;
-	let mut y: i16 = 0;
-	houses.insert((x, y));
-	for character in characters {
-		match character {
-			'<' => x -= 1,
-			'>' => x += 1,
-			'v' => y -= 1,
-			'^' => y += 1,
+	let mut x: Vec<i16> = vec![0; santas];
+	let mut y: Vec<i16> = vec![0; santas];
+	houses.insert((0, 0));
+	for c in 0..characters.len() {
+		let i = c % santas;
+		match characters.get(c..c + 1).unwrap() {
+			"<" => x[i] -= 1,
+			">" => x[i] += 1,
+			"v" => y[i] -= 1,
+			"^" => y[i] += 1,
 			_ => panic!(""),
 		}
-		houses.insert((x, y));
+		houses.insert((x[i], y[i]));
+		houses.insert((x[i], y[i]));
 	}
-	println!("The result for the first part is: {}", houses.len());
+	houses.len()
 }
